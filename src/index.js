a8f117c0bfc955f10a73a0cff992ef40795507c8
Alan Alvarez <alandanielalvarez0000@gmail.com>
2021-03-14 20:59:42 -0600

    Se configuro el index.js y se importaron los modulos a utilizar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
-
+const path = require('path');
+const morgan = require('morgan');
+const multer = require('multer');
 // Initializations 
 const app = express();
 
 // Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname,'views')); // Se especifica donde está la carpeta views 
+app.set('view engine', 'ejs'); // Se configura ejs con el render engine 
+
 
 //Middlewares 
+app.use(morgan('dev')); // Imprime en consola las peticiones que se le hace al servidor 
+app.use(express.urlencoded({extended: false})); 
+app.use(multer({dest: path.join(__dirname, 'img/uploads')}).single('image')); // Configura el directorio donde multer va a guardar imagenes 
 
-//Routes
+
 
 //Global Variables
+
+//Routes
+app.use(require('./routes/index'));
+
 
 //Static Files
 
 //Start server
-app.listen(3000, () => {
-    console.log(`Server on port ${3000}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = require('express'); // Se con figura el router que se importara
+
+const router = Router(); // Guarda la información en un objecto llamado router
+
+module.exports = router; // exporta el objeto router 
