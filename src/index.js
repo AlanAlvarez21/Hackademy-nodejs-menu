97895f2262263cb163c1c55c90169394b8b302aa
Alan Alvarez <alandanielalvarez0000@gmail.com>
2021-03-14 20:42:06 -0600

    Se inicializo servidor y se se crearon carpetas

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Initializations 
+const app = express();
+
+// Settings
+
+//Middlewares 
+
+//Routes
+
+//Global Variables
+
+//Static Files
+
+//Start server
+app.listen(3000, () => {
+    console.log(`Server on port ${3000}`);
+});
