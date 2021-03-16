23725af540f0a25d84b185d6c1241b5fc55a4472
Alan Alvarez <alandanielalvarez0000@gmail.com>
2021-03-15 18:47:32 -0600

    App conectada a cloudinary, ahora las imagenes se suben a la nube

--- src/models/Image.js
+++ src/models/Image.js
@@ -11,6 +11,9 @@ const imageSchema = new Schema({
     originalname: { type: String },
     size: { type: Number },
     created_at: { type: Date, default: Date.now() },
+    imageURL: {type: String},  // La dirección que otorga cloudinary 
+    public_id: {type: String}// identificador otorgado por cloudinary, de está manera se mantiene un id unico por imagen
+
 });
 
 module.exports = model('Image', imageSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,15 @@ const router = Router(); // Guarda la información en un objecto llamado router
 const { unlink } = require('fs-extra');
 const Image = require('../models/Image'); // Importa el modelo de imagenes 
 const path = require('path');
+const cloudinary = require('cloudinary');
+
+//Se configuran las Keys para conectar a cloudinary con variables de entorno 
+cloudinary.config({
+    cloud_name: 'dssrbmpy2',
+    api_key: '621849932743623',
+    api_secret: 'k-hVyH11sYE1mXhVab5TWp3D2cA'
+})
+
 
 router.get('/', async (req, res) => {
     const images = await Image.find();
@@ -24,6 +33,8 @@ router.get('/image/:id', async (req, res) => { // Ruta ver una determinada image
 
 router.post('/upload/', async (req, res) => { // Ruta para subir imagenes 
     //console.log(req.file); Imprime en consola el Json con los datos de a imagen subida 
+    const {title, description} = req.body; // desde request body se extrae el titulo y la descripcion 
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
     const image = new Image();
     image.title = req.body.title; // Recibe el titulo de la imagen
     image.description = req.body.description;  // Recibe la descripcion de la imagen
@@ -32,8 +43,10 @@ router.post('/upload/', async (req, res) => { // Ruta para subir imagenes
     image.originalname = req.file.originalname;
     image.mimetype = req.file.mimetype;
     image.size = req.file.size;
-
+    image.imageURL = result.url;
+    image.public_id = result.public_id;
     await image.save(); // Guarda objeto recien creado    
+    await unlink(path.resolve('./src/public' + image.path)); // Elimina la imagen en base a su ruta 
     console.log(image); //Imprime objeto imagen creado
     res.redirect('/');
 });
@@ -41,7 +54,7 @@ router.post('/upload/', async (req, res) => { // Ruta para subir imagenes
 router.get('/image/:id/delete', async (req, res) => { // Ruta para eliminar imagenes 
     const { id } = req.params; // Se obtiene el id de la imagen
     const image =  await Image.findByIdAndDelete(id); // Busca el id y elimina archivo relacionado 
-    await unlink(path.resolve('./src/public' + image.path)); // Elimina la imagen en base a su ruta 
+    const result = await cloudinary.v2.uploader.destroy(image.public_id);
     res.redirect('/');
 });
 
